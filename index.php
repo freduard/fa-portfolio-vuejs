201720d4661eac038f05ed1fb36e0679ec74dd3e
Fredy Ait <aitfredy@gmail.com>
2022-08-19 12:15:09 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $honeyPotMail = $_POST['email'];
+    $honeyPotWebsite = $_POST['website'];
+    $subject = "New message from portfolio";
+    $mailFrom = $_POST['emanueladdresshaha'];
+    $message = $_POST['message'];
+    $mailTo = "server@tak21fredyait.itmajakas.ee";
+    $headers = "Sent by ".$name." at ".$mailFrom;
+
+    if (empty($honeyPotMail) && empty($honeyPotWebsite)) {
+        mail($mailTo, $subject, $message, $headers);
+        header("Location: https://fredyait.com/Contact.vue?state=1");
+    } else {
+        header("Location: https://fredyait.com");
+    }
+}
