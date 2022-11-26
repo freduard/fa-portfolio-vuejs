e4a229c5eea3745e8b11bd9e58a9344409474262
“Fredy <aitfredy@gmail.com>
2022-11-26 16:05:23 +0200

    Converted script from Vue2 to Vue3

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,27 @@
+<script setup>
+import { projects } from '../projects.js'
+import { getCurrentInstance } from 'vue'
+
+const instance = getCurrentInstance();
+
+let sortByLatest = true;
+
+function getShortDesc (pIndex) {
+    return projects[pIndex].p1.substring(0, 100) + '...';
+}
+
+function sortBy() {
+    projects.reverse();
+    instance.proxy.$forceUpdate();
+    sortByLatest = sortByLatest ? false : true;
+    document.getElementById('sortButton').innerText = sortByLatest ? 'Hilisemad' : 'Varasemad';
+}
+</script>
+
 <template>
     <div class="flex justify-between mb-6 items-center">
         <p class="text-2xl lg:text-6xl">Projektid</p>
-        <button v-on:click="sortBy" id="sortButton" class="bg-zinc-700 p-2 rounded hover:bg-slate-600 duration-150">{{sortByLatest ? 'Hilisemad' : 'Varasemad'}}</button>
+        <button v-on:click="sortBy" id="sortButton" class="bg-zinc-700 p-2 rounded hover:bg-slate-600 duration-150">Hilisemad</button>
     </div>
     <div>
         <router-link :to="{ path: '/Project.vue', query: { projectIndex: projects.findIndex(item => item.id === project.id) }}" v-for="(project, index) in projects" :key="index" class="duration-300 ease-in-out bg-zinc-700 rounded drop-shadow-md hover:bg-slate-600 flex flex-col lg:flex-row lg:h-36 mb-6">
@@ -29,31 +49,3 @@
     filter: saturate(1);
 }
 </style>
-
-<script>
-import { projects } from '../projects.js'
-
-export default {
-    data() {
-        return {
-            sortByLatest: true,
-        }
-    },
-    methods: {
-        getShortDesc (pIndex) {
-            return projects[pIndex].p1.substring(0, 100) + '...';
-        },
-
-        sortBy() {
-            projects.reverse();
-            this.$forceUpdate();
-            this.sortByLatest = this.sortByLatest ? false : true;
-        }
-    },
-    setup() {
-        return { projects };
-    }
-}
-
-
-</script>
