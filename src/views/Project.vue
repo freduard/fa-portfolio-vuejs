a751ca13f46cdf4dca6961d1cdbd638a6055a143
“Fredy <aitfredy@gmail.com>
2022-11-28 09:32:15 +0200

    Split project navigator into component

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,12 +1,6 @@
 <template>
     <div class="flex flex-col gap-6">
-        <div class="flex justify-between items-center">
-            <router-link :to="{ path: '/Project.vue', query: { projectIndex: projectNavigator(false) }}" class="hidden lg:flex bg-zinc-700 hover:bg-slate-600 p-2 rounded items-center justify-center gap-2 duration-150"><ArrowLeftIcon class="w-5 mb-[1px]"/> {{ getProjectTitle(false) }}</router-link>
-            <router-link :to="{ path: '/Project.vue', query: { projectIndex: projectNavigator(false) }}" class="lg:hidden flex items-center justify-center gap-2 duration-150 bg-zinc-700 hover:bg-slate-600 p-3 rounded"><ArrowLeftIcon class="w-4"/></router-link>
-            <router-link to="/Projects.vue" class="bg-zinc-700 hover:bg-slate-600 p-3 rounded"><ArrowUpIcon class="w-4" /></router-link>
-            <router-link :to="{ path: '/Project.vue', query: { projectIndex: projectNavigator(true) }}" class="hidden lg:flex bg-zinc-700 hover:bg-slate-600 p-2 rounded items-center justify-center gap-2 duration-150">{{ getProjectTitle(true) }} <ArrowRightIcon class="w-5 mb-[1px]" /></router-link>
-            <router-link :to="{ path: '/Project.vue', query: { projectIndex: projectNavigator(true) }}" class="lg:hidden flex items-center justify-center gap-2 duration-150 bg-zinc-700 hover:bg-slate-600 p-3 rounded"><ArrowRightIcon class="w-4" /></router-link>
-        </div>
+        <ProjectNavVue :projectIndex="props.projectIndex" />
         <div class="max-h-96 flex justify-center items-center overflow-hidden">
             <img :src="project.image" class="w-full rounded lg:h-80" style="object-fit: cover;" alt="">
         </div>
@@ -34,53 +28,12 @@
 
 <script setup>
 import { projects } from '../projects.js'
-import { ArrowRightIcon } from '@heroicons/vue/outline'
-import { ArrowLeftIcon } from '@heroicons/vue/outline'
-import { ArrowUpIcon } from '@heroicons/vue/outline'
+import ProjectNavVue from '../components/ProjectNav.vue'
 
 const project = projects[props.projectIndex]
 const attrLength = Object.keys(project).length - 7
-const nextProjectTitle = "";
 
 const props = defineProps({
     projectIndex: String
 })
-
-function projectNavigator(direction){
-    if(direction){
-        if(props.projectIndex != (projects.length - 1)){
-            return projects.findIndex(item => item.id === project.id) + 1;
-        } else{
-            return 0;
-        }
-    }
-    else{
-        if(props.projectIndex != 0){
-            return projects.findIndex(item => item.id === project.id) - 1;
-        } else{
-            return (projects.length - 1);
-        }
-    }
-
-}
-
-function getProjectTitle(direction){
-    if(direction){
-        if(props.projectIndex != (projects.length - 1)){
-            return projects[parseInt(props.projectIndex) + 1].title.substring(0, 16) + "...";
-        } else{
-            return projects[0].title.substring(0, 16) + "...";
-        }
-    } 
-    else{
-        if(props.projectIndex != 0){
-            return projects[parseInt(props.projectIndex) - 1].title.substring(0, 16) + "...";
-        } else{
-            return projects[projects.length - 1].title.substring(0, 16) + "...";
-        }
-    }
-
-}
-
-
 </script>
