2343e94745c5c56d90aa4f99acef7fe5b34b5b07
“Fredy <aitfredy@gmail.com>
2022-11-28 09:32:24 +0200

    Script setup on top

--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { documents } from '../documents.js'
+</script>
+
 <template>
     <div class="flex justify-between mb-6 items-center">
         <p class="text-2xl lg:text-6xl mr-[]">Dokumendid</p>
@@ -9,7 +13,3 @@
         </div>
     </div>
 </template>
-
-<script setup>
-import { documents } from '../documents.js'
-</script>
