43c805d2844aba80f21c5d9adc4e4d161c307ff1
Fredy Ait <aitfredy@gmail.com>
2022-12-05 02:43:59 +0200

    Fixed mobile view header

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import SideNavVue from './components/SideNav.vue';
 
 <template>
   <img src="./assets/images/bmw.jpg" alt="bmw" id="bgImage" class="fixed h-screen w-screen" style="object-fit: cover; filter: brightness(0.5) blur(100px) saturate(0.5); scale: 1.5;">
-  <div class="h-screen w-screen bg-zinc-900 p-4 lg:p-12 grid grid-rows-6 lg:grid-cols-3 lg:grid-rows-none gap-4 lg:gap-12">
+  <div class="h-screen w-screen bg-zinc-900 p-4 lg:p-12 grid grid-rows-10 lg:grid-cols-3 lg:grid-rows-none gap-4 lg:gap-12">
       <SideNavVue />
 
-      <div class="lg:flex lg:col-span-2 row-span-5 lg:row-span-1 bg-zinc-800 h-full rounded drop-shadow-md p-4 lg:p-12 flex-col justify-between items-center overflow-x-hidden componentWrapper">
+      <div class="lg:flex lg:col-span-2 row-span-6 lg:row-span-1 bg-zinc-800 h-full rounded drop-shadow-md p-4 lg:p-12 flex-col justify-between items-center overflow-x-hidden componentWrapper">
         <router-view v-slot="{ Component, route }">
           <transition name="route" mode="out-in">
             <div :key="route.fullPath">
