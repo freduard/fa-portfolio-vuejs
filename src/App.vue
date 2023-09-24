ee1c416637999ddabd9b5f443566c57528389037
Fredy Ait <aitfredy@gmail.com>
2023-09-25 00:30:08 +0300

    test #3

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@ onMounted(() => {
     <Header position="fixed" :current-section="currentSection" />
     
     <swiper 
-    class="h-[100dvh] w-screen"
+    class="h-screen w-screen test"
     :hash-navigation="{
       watchState: true,
     }"
