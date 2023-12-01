2db708d0efe243ea0a45fb9317c7d94ca8ea785c
Fredy Ait <aitfredy@gmail.com>
2023-12-01 15:46:37 +0200

    finished services page & fixed bgu

---
+++ src/services.js
@@ -0,0 +1,26 @@
+export const services = [
+    {
+        id: 0,
+        title: 'Custom Script',
+        priceRange: '€25 - €200',
+        description: 'I will write you a custom script for any of your needs.'
+    },
+    {
+        id: 1,
+        title: 'Custom Portfolio',
+        priceRange: '€150 - €500',
+        description: 'I will make you a custom portfolio. Price depends on the amount of content & features required.'
+    },
+    {
+        id: 2,
+        title: 'Full-stack Website',
+        priceRange: '€650 - €2500',
+        description: 'A full stack website like a store or some other management application.'
+    },
+    {
+        id: 3,
+        title: 'Windows Application',
+        priceRange: '€250 - €2000',
+        description: 'I will make you a Windows application. Price depends on functionality and complexity.'
+    },
+]
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,14 +1,25 @@
 <script setup>
-
+import { services } from '../services'
 </script>
 
 <template>
-    <div class="w-full h-full overflow-hidden">
+    <div class="w-full h-full overflow-hidden text-white">
         <div class="px-12 md:px-24 lg:px-36 2xl:px-48 w-full h-full">
-            <h1 class="text-6xl sm:text-7xl lg:text-8xl 2xl:text-9xl font-bold py-24 lg:py-28 xl:py-32 text-white font-['mrdafoe'] ">
+            <h1 class="text-6xl sm:text-7xl lg:text-8xl 2xl:text-9xl font-bold py-16 lg:py-28 xl:py-32 font-['mrdafoe']">
                 Services
             </h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 max-h-[750px] overflow-auto py-24 pb-44 lg:pb-0 sm:py-32 lg:py-24 xl:py-12 gap-6 lg:gap-12 font-['quicksand'] px-4">
+                <div v-for="(service, index) in services" :key="index" class="bg-zinc-900 rounded-xl border border-neutral-800 drop-shadow-cust p-6 -z-20 lg:z-0 hover:brightness-110 hover:scale-105 duration-150 gap-2 flex flex-col">
+                    <div>
+                        <h1 class="text-base md:text-xl font-bold">{{ service.title }}</h1>
+                        <p>{{ service.priceRange }}</p>
+                    </div>
+                    <p class="text-sm lg:text-base">{{ service.description }}</p>
+                </div>
+            </div>
         </div>
-        <div class="absolute -top-64 -left-12 h-1/2 -rotate-12 -z-50 scale-125 w-full bg-stone-800" />
+        <div class="absolute -top-64  h-1/2 rotate-6 scale-125 w-full bg-zinc-900 drop-shadow-md -z-10" />
+        <div class="absolute -top-64 lg:-top-44 -left-12 h-96 -rotate-12 scale-125 w-full bg-stone-800 drop-shadow-md -z-10" />
+        
     </div>
 </template>
